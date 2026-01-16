4c7c21653836e63917382fa4aa84ad3c0b9c65a6
Ayush Kumar Jha B <rookie@Ayushs-MacBook-Air.local>
2026-01-16 14:18:43 +0530

    connecting to socket Server

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -1,5 +1,15 @@
 import React, { useEffect, useRef } from "react";
 import server from "../environment";
+
+const server_url=server;
+
+var connections={};
+
+const peerConfigConnections={ //helps in establishing the peerConnections with other clients helps the client to puch through the network to reach you
+    "iceServers":[
+        {"urls":"stun:stun.l.google.com:19302"} // helps in determing the user public ip of the client for conenctions
+    ]
+}
  
 
 export default function VideoMeetComponent(){
@@ -13,7 +23,7 @@ export default function VideoMeetComponent(){
 
     let [audioAvailable, setAudioAvailable] = useState(true);   //browser Permission
 
-    let [video, setVideo] = useState([]);   //userVideo
+    let [video, setVideo] = useState([]);   //userVideo status
 
     let [audio, setAudio] = useState(); //userAudio
 
@@ -33,7 +43,7 @@ export default function VideoMeetComponent(){
 
     let [username, setUsername] = useState("");
 
-    const videoRef = useRef([])
+    const videoRef = useRef([]) //you are saving the latest list of videos in a place where your event listeners can instantly read it without waiting for React to re-render.
 
     let [videos, setVideos] = useState([]);
 
@@ -69,7 +79,7 @@ export default function VideoMeetComponent(){
                 setScreenAvailable(false);
             }
 
-            if(videoAvailable || audioAvailable){ //displaying the user video to user itself
+            if(videoAvailable || audioAvailable){ //displaying the user video to user itself //if both are false then we dont need to send the streams
                 const userMediaStream=await navigator.mediaDevices.getUserMedia({video:videoAvailable, audio:audioAvailable});
                 if(userMediaStream){  
                     window.localStream=userMediaStream;
@@ -81,6 +91,136 @@ export default function VideoMeetComponent(){
         }catch(error){
             console.log(error);
         }
+    }
+
+    //this useEffect runs everytime the user (toggles the mute or unmuted(video as well) and asks the hardware to update)
+    useEffect(()=>{
+        if(video !== undefined && audio !== undefined){
+            getUserMedia();
+        }
+    },[video,audio]);
+
+    let getMedia=()=>{ //runs after the connect function of the lobby conenct button
+        setVideo(videoAvailable);
+        setAudio(audioAvailable);
+        connectToSocketServer();
+    }
+
+    let getUserMediaSucess= (stream)=>{
+
+    }
+
+    let getUserMedia= ()=>{
+        if((video && videoAvailable) || (audio && audioAvailable)){
+            navigator.mediaDevices.getUserMedia({video:video,audio:audio}) //update the hardware settings
+            .then(getUserMediaSucess) //passess the fresh stream to the fucntion (which updates the stream and sends new stream to other people on the call)
+            .then((stream)=>{ })
+            .catch((e)=>console.log(e))
+
+        }else{//The "Turn Off" Block
+            try{
+                let tracks=localVideoref.current.srcObject.getTracks();// grabs the current stream from the video tag
+                tracks.forEach(track=>track.stop()) // stops the track so that if other apps want to use it and turns of the video light of the computer hardware
+            }catch(e){ }
+        }
+    }
+
+    let DisplayMediaSuccess =(stream)=>{
+
+    }
+
+    let connectToSocketServer=()=>{
+        socketRef.current =io.connect(server_url, {secure:false})
+
+        socketRef.current.on("signal", gotMessageFromServer)
+
+        socketRef.current.on("connect",()=>{
+            socketRef.current.emit("join-call", window.location.href); //sending the user the path
+            SocketIdRef.current=socketRef.current.id;
+
+            socketRef.current.on("chat-message",addMessage)
+
+            socketRef.current.on("user-left",(id)=>{//removing the video of the client which left from the frontend
+                setVideos((video) => videos.filter((video)=> video.socketId !==id))
+            })
+            
+            socketRef.current.on("user-joined",(id,clients)=>{ //socket.id, connections[path]) from backend
+                clients.forEach((socketListId)=>{
+                    connections[socketListId]=new RTCPeerConnection(peerConfigConnections); //create a new RTC communication using the stun server
+                    
+                    //Browser is looking for any possible address where data can be sent to reach your computer.
+                    connections[socketListId].onicecandidate=function(event){ //{"new path" (an ICE Candidate)}
+                        if(event.candidate != null){
+                            socketRef.current.emit("signal",socketListId,JSON.stringify({"ice":event.candidate})) //pass the iceCandidate path to the client
+                        }
+                    }
+
+                    //waiting for the user streams
+                    connections[socketListId].onaddstream=(event)=>{
+                        let videoExists =videoRef.current.find(video=>video.socketId === socketListId);
+
+                        if(videoExists){
+                            console.log("FOUND EXISITING");
+
+                            //updating the stream of the existing video
+                            setVideos(videos=>{// we are creating a new copy,becase the memory address changes and triggers the react to re render
+                                const updatedVideos=videos.map(video=>
+                                    video.socketId===socketListId ?{ ...video,stream:event.stream}:video //To keep their ID consistent while swapping their old video feed for the new one,
+                                    //  and to force React to update the screen.
+                                );
+                                videoRef.current=updatedVideos;
+                                return updatedVideos;
+                            });
+                        }else{
+                            //create a new video
+                            console.log("creating New");
+                            let newVideo={
+                                socketId:socketListId,
+                                stream:event.stream,
+                                autoPlay:true,
+                                playsinline:true
+                            };
+
+                            setVideos(videos=>{
+                                const updatedVideos=[...videos,newVideo];
+                                videoRef.current=updatedVideos;
+                                return updatedVideos;
+                            });
+                        }
+                    }
+
+                    //adding the localstream to the webRTC to share the stream to the peers
+                    if(window.localStream !==undefined && window.localStream !==null){//do i currently have a working camera and microphone stream
+                        connections[socketListId].addStream(window.localStream); //send the stream to all the peers
+                    }else{
+                        //in webRTC if we initiate a connection with zero audio or video Tracks the conneciton negatiation falisl
+                        //hence we create a fake stream
+                        let blackSilence=(...args)=>new MediaStream([balck(...args),silence()]);
+                        window.localStream=blackSilence()
+                        connections[socketListId].addStream(window.localStream)
+
+                    }
+                })
+
+                if(id===SocketIdRef.current){//is the person who joined the room me ? yes call everyone else
+                    for(let id2 in connections){//loop through every person in the room
+                        if(id2 ===SocketIdRef===current) continue; //skip Myself i dont to establish connection with myself
+
+                        try{//defensive move to add the stream again (defensive) webRTC requires stream to be added before creating an offer
+                            connections[id2].addStream(window.localStream)
+                        }catch(e){ }
+
+                        connections[id2].createOffer().then((description)=>{
+                            connections[id2].setLocalDescription(description)
+                            .then(()=>{
+                                socketRef.current.emit("signal",id2,JSON.stringify({"sdp":connections[id2].LocalDescription})) //send the payload to the receipt
+                            })
+                            .catch(e=>console.log(e));
+                        })
+                    }
+                }
+            })
+        })
     }
 
 
